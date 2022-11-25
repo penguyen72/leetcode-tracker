e6d9a2c78a2f24aae5b80c864534dcfbcf5be0f8
penguyen <peytonn72@gmail.com>
2022-11-25 00:01:11 -0500

    create loading dialog and refactor code

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -8,6 +8,13 @@ import Grid from "@mui/material/Grid";
 import LoadingButton from "@mui/lab/LoadingButton";
 import SaveIcon from "@mui/icons-material/Save";
 import { stepLabelClasses } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
 const { Client } = require("@notionhq/client");
 
@@ -18,8 +25,66 @@ const notion = new Client({
 const Home: NextPage = () => {
   const [problemLink, setProblemLink] = useState("");
   const [linkError, setLinkError] = useState(false);
+  const [fetchDialog, setFetchDialog] = useState(false);
   const [secretKey, setSecretKey] = useState("");
   const [databaseID, setDatabaseID] = useState("");
+
+  const obtainInformation = (leetcodeURL: string) => {
+    return new Promise((resolve, reject) => {
+      fetch("/api/submitQuestion", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: leetcodeURL }),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  const buildData = () => {
+    return {
+      parent: {
+        type: "database_id",
+        database_id: process.env.NOTION_DB,
+      },
+      properties: {
+        Number: {
+          number: 1,
+        },
+        "Problem Name": {
+          title: [
+            {
+              text: {
+                content: "Problem Name",
+              },
+            },
+          ],
+        },
+      },
+    };
+  };
+
+  const buildOptions = (data: any) => {
+    return {
+      method: "POST",
+      headers: {
+        mode: "no-cors",
+        accept: "application/json",
+        "Notion-Version": "2022-06-28",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  };
 
   function createPage(leetcodeURL: string) {
     fetch("/api/submitQuestion", {
@@ -42,36 +107,6 @@ const Home: NextPage = () => {
 
   // const createPage = () => {
   //   return new Promise((resolve, reject) => {
-  //     const data = {
-  //       parent: {
-  //         type: "database_id",
-  //         database_id: process.env.NOTION_DB,
-  //       },
-  //       properties: {
-  //         Number: {
-  //           number: 1,
-  //         },
-  //         "Problem Name": {
-  //           title: [
-  //             {
-  //               text: {
-  //                 content: "Problem Name",
-  //               },
-  //             },
-  //           ],
-  //         },
-  //       },
-  //     };
-  //     const options = {
-  //       method: "POST",
-  //       headers: {
-  //         mode: "no-cors",
-  //         accept: "application/json",
-  //         "Notion-Version": "2022-06-28",
-  //         "content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(data),
-  //     };
 
   //     fetch("https://api.notion.com/v1/pages", options)
   //       .then((response) => response.json())
@@ -87,15 +122,24 @@ const Home: NextPage = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
     if (validateLink(problemLink)) {
-      createPage(problemLink);
+      setFetchDialog(true);
+      obtainInformation(problemLink)
+        .then((response) => {
+          console.log(response);
+          setFetchDialog(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setFetchDialog(false);
+        });
     } else {
       setLinkError(true);
     }
   };
 
-  // useEffect(() => {
-  //   console.log(problemLink);
-  // }, [problemLink]);
+  const handleClose = () => {
+    setLinkError(false);
+  };
 
   return (
     <div className={styles.container}>
@@ -110,17 +154,20 @@ const Home: NextPage = () => {
             setProblemLink(e.target.value);
           }}
         />
-        {/* <LoadingButton
-        loading
-        loadingPosition="start"
-        startIcon={<SaveIcon />}
-        variant="outlined"
-      >
-        Uploading
-      </LoadingButton> */}
         <Button variant="outlined" onClick={handleSubmit}>
           Submit
         </Button>
+        <Snackbar open={linkError} onClose={handleClose}>
+          <Alert severity="error">Please submit a proper link!</Alert>
+        </Snackbar>
+        <Dialog open={fetchDialog}>
+          <DialogTitle>Please Wait...</DialogTitle>
+
+          <CircularProgress />
+          <DialogContentText id="alert-dialog-description">
+            We are currently obtaining information on Leetcode.com
+          </DialogContentText>
+        </Dialog>
         {/* <form onSubmit={handleSubmit}>
           <label>Insert Leetcode Problem Link</label>
           <input type="text" id="problem" name="problem"></input>
@@ -129,56 +176,6 @@ const Home: NextPage = () => {
         </form> */}
       </div>
     </div>
-    // <div>
-    //   <TextField
-    //     id="outlined-basic"
-    //     label="Outlined"
-    //     variant="outlined"
-    //     helperText="Insert Leetcode Link"
-    //     fullWidth={true}
-    //   />
-    //   {/* <LoadingButton
-    //     loading
-    //     loadingPosition="start"
-    //     startIcon={<SaveIcon />}
-    //     variant="outlined"
-    //   >
-    //     Uploading
-    //   </LoadingButton> */}
-    //   <Button variant="outlined">Outlined</Button>
-    //   <form onSubmit={handleSubmit}>
-    //     <label>Insert Leetcode Problem Link</label>
-    //     <input type="text" id="problem" name="problem"></input>
-    //     <br />
-    //     <button type="submit">Submit</button>
-    //   </form>
-    // </div>
-    // <div className={styles.component}>
-    //   <div className={styles.information}>
-    //     <TextField
-    //       id="outlined-basic"
-    //       label="Outlined"
-    //       variant="outlined"
-    //       helperText="Insert Leetcode Link"
-    //       fullWidth={true}
-    //     />
-    //     {/* <LoadingButton
-    //     loading
-    //     loadingPosition="start"
-    //     startIcon={<SaveIcon />}
-    //     variant="outlined"
-    //   >
-    //     Uploading
-    //   </LoadingButton> */}
-    //     <Button variant="outlined">Outlined</Button>
-    //     <form onSubmit={handleSubmit}>
-    //       <label>Insert Leetcode Problem Link</label>
-    //       <input type="text" id="problem" name="problem"></input>
-    //       <br />
-    //       <button type="submit">Submit</button>
-    //     </form>
-    //   </div>
-    // </div>
   );
 };
 
